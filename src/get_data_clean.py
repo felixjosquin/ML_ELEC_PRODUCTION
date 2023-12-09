73d3dcedfda85312d2202d8bc9650c866dd0c736
felixjosquin <fj.josquin@gmail.com>
2023-12-09 23:29:05 +0100

    creat csv with clean data

---
+++ src/get_data_clean.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+
+
+csv_file_paths = [
+    "./data/CO2mix_RTE_2019.csv",
+    "./data/CO2mix_RTE_2020.csv",
+    "./data/CO2mix_RTE_2022.csv",
+]
+data_clean_path = "./data/data.csv"
+
+
+for csv_file in csv_file_paths:
+    df = pd.read_csv(csv_file, sep=";")
+    df_data = df.loc[
+        df.index % 2 == 0,
+        [
+            "Date",
+            "Heures",
+            "Fioul",
+            "Charbon",
+            "Gaz",
+            "Nucléaire",
+            "Eolien",
+            "Solaire",
+            "Hydraulique",
+            "Bioénergies",
+            "Consommation",
+            "Prévision J-1",
+        ],
+    ]
+    df_data.rename(columns={"Heures": "Start_time"}, inplace=True)
+    df_data.insert(
+        df_data.columns.get_loc("Start_time") + 1,
+        "End_time",
+        df.loc[df.index % 2 == 1, "Heures"].to_list(),
+    )
+    df_data["Start_time"] = pd.to_datetime(
+        df_data["Date"] + " " + df_data["Start_time"], format="%d/%m/%Y %H:%M"
+    )
+    df_data["End_time"] = pd.to_datetime(
+        df_data["Date"] + " " + df_data["End_time"], format="%d/%m/%Y %H:%M"
+    )
+    df_data = df_data.drop(columns=["Date"])
+
+    df_data.to_csv(
+        data_clean_path,
+        mode="a",
+        index=False,
+        header=not os.path.exists(data_clean_path),
+    )
+
+    # df_data.loc.plot(kind="line", x="Start_time", y=["Nucléaire"])
+    # plt.savefig("plot/myimage.png", dpi=1200)
