cfa8be545df11accfde881d8730fdb0a8e61d613
felixjosquin <fj.josquin@gmail.com>
2023-12-14 23:01:20 +0100

    add register model + add prevision plot on model1

--- model1.py
+++ model1.py
@@ -1,20 +1,29 @@
 import sys
 import pandas as pd
 import numpy as np
-from tqdm import tqdm
 import matplotlib.pyplot as plt
 
 import torch
 import torch.nn as nn
 
-nb_days_cnn = 4
-nb_epoch = 300
-hidden_layer = 40
-crit = nn.MSELoss()
+###### HYPERPARAMETER ######
+nb_epoch = 500
+lr = 0.0001
+hidden_layer = 120
+nb_days_cnn = 6
+nb_days_by_batch = 73  # 1 5 3 15 73 219 365 1095 (values possible)
+#############################
 
-NB_RECORD_FOR_DAY = 12
+NB_RECORD_FOR_DAY = 48
 NB_CHANNELS = 8
-NB_DAYS_BY_BATCH = 20
+
+
+def crit(output, target):
+    # output = nb_days * ( NB_RECORD_FOR_DAY x NB_CHANNELS)
+    loss_vector = (output - target) ** 2
+    loss_vector = torch.sum(loss_vector, dim=1)
+    return torch.mean(loss_vector)
+
 
 df = pd.read_csv(
     "./data/data.csv",
@@ -31,31 +40,35 @@ keywords = [
     "Hydraulique",
     "Bioénergies",
 ]
-print(df[keywords].max())
-df[keywords] /= df[keywords].max()
+df_max = df[keywords].max()
+df[keywords] /= df_max[keywords]
 
-traindata = (
-    df.loc[df["End_time"] < "2021-12-31"]
-    .drop(
-        columns=[
-            "Start_time",
-            "End_time",
-        ]
-    )
-    .values
+traindata = np.array(
+    [
+        df.loc[df["End_time"] <= "2021-12-31"]
+        .drop(
+            columns=[
+                "Start_time",
+                "End_time",
+            ]
+        )
+        .values
+    ]
 )
-print(traindata.shape)
-
-assert traindata.shape[0] % NB_DAYS_BY_BATCH * NB_RECORD_FOR_DAY == 0
 traindata = np.reshape(
-    traindata, (-1, NB_DAYS_BY_BATCH * NB_RECORD_FOR_DAY, NB_CHANNELS)
+    traindata,
+    (
+        traindata.shape[1] // (nb_days_by_batch * NB_RECORD_FOR_DAY),
+        nb_days_by_batch * NB_RECORD_FOR_DAY,
+        NB_CHANNELS,
+    ),
 )
-nb_days_batch = (traindata.shape[1] // NB_RECORD_FOR_DAY) - nb_days_cnn
 
 trainx = torch.tensor(traindata[:, :-NB_RECORD_FOR_DAY, :], dtype=torch.float32)
 trainy = torch.tensor(
     traindata[:, nb_days_cnn * NB_RECORD_FOR_DAY :, :], dtype=torch.float32
 )
+nb_day_batch_train = trainy.shape[1] // NB_RECORD_FOR_DAY
 trainds = torch.utils.data.TensorDataset(trainx, trainy)
 trainloader = torch.utils.data.DataLoader(trainds, batch_size=1, shuffle=False)
 
@@ -73,6 +86,7 @@ testx = torch.tensor(np.array([testdata[:-NB_RECORD_FOR_DAY, :]]), dtype=torch.f
 testy = torch.tensor(
     np.array([testdata[nb_days_cnn * NB_RECORD_FOR_DAY :, :]]), dtype=torch.float32
 )
+nb_day_batch_test = testy.shape[1] // NB_RECORD_FOR_DAY
 testds = torch.utils.data.TensorDataset(testx, testy)
 testloader = torch.utils.data.DataLoader(testds, batch_size=1, shuffle=False)
 
@@ -88,6 +102,7 @@ class Mod(nn.Module):
             stride=NB_RECORD_FOR_DAY,
         )
         self.sigmoid = nn.Sigmoid()
+        self.reLU = nn.ReLU()
         self.mlp = nn.Linear(nhid, NB_RECORD_FOR_DAY * NB_CHANNELS)
 
     def forward(self, x):
@@ -95,43 +110,44 @@ class Mod(nn.Module):
         x = torch.transpose(x, 1, 2)
         x = self.cnn(x)
         y = self.sigmoid(x)
-        B, H, T = y.shape
+        B, C_out, L_out = y.shape
+        # y = B * C_out * L_out
         yy = y.transpose(1, 2)
-        # yy = B, T, d
-        y = self.mlp(yy.view(B * T, H))
-        return y.view(-1, NB_CHANNELS)
+        y = self.mlp(yy.view(B * L_out, C_out))
+        return y.view(-1, NB_RECORD_FOR_DAY * NB_CHANNELS)
 
 
 def test(mod):
     mod.train(False)
-    totloss, nbatch = 0.0, 0
+    totloss, nb_batch = 0.0, 0
     for inputs, goldy in testloader:
         haty = mod(inputs)
-        goldy = goldy.view(-1, NB_CHANNELS)
+        goldy = goldy.view(-1, NB_RECORD_FOR_DAY * NB_CHANNELS)
         loss = crit(haty, goldy)
         totloss += loss.item()
-        nbatch += nb_days_batch
-    totloss /= float(nbatch)
+        nb_batch += 1
+    totloss /= float(nb_batch)
     mod.train(True)
     return totloss
 
 
 def train(mod):
-    optim = torch.optim.Adam(mod.parameters(), lr=0.001)
+    optim = torch.optim.Adam(mod.parameters(), lr=lr)
     train_loss_data, test_loss_data = [], []
     for epoch in range(nb_epoch):
-        totloss, nb_days = 0.0, 0
+        totloss, nb_batch = 0.0, 0
+        testloss = test(mod)
         for inputs, goldy in trainloader:
             optim.zero_grad()
-            goldy = goldy.view(-1, NB_CHANNELS)
+            # goldy = 1 * batch_size * NB_CHANNELS
+            goldy = goldy.view(-1, NB_RECORD_FOR_DAY * NB_CHANNELS)
             haty = mod(inputs)
             loss = crit(haty, goldy)
             totloss += loss.item()
-            nb_days += nb_days_batch
+            nb_batch += 1
             loss.backward()
             optim.step()
-        totloss /= float(nb_days)
-        testloss = test(mod)
+        totloss /= float(nb_batch)
         print(
             f"epoch {epoch+1}/{nb_epoch} | train_loss: {totloss} | test_loss: {testloss}"
         )
@@ -151,3 +167,34 @@ print(
     file=sys.stderr,
 )
 train(mod)
+torch.save(mod.state_dict(), "./data/model.pt")
+
+mod.load_state_dict(torch.load("data/model.pt"))
+input, goldy = next(iter(trainloader))
+haty = (
+    np.transpose(mod(input).view(-1, NB_CHANNELS).detach().numpy())
+    * df_max.values[:, np.newaxis]
+)
+goldy = (
+    np.transpose(goldy.view(-1, NB_CHANNELS).detach().numpy())
+    * df_max.values[:, np.newaxis]
+)
+nb_days_show = 15
+abscisse = np.linspace(0, nb_days_show, nb_days_show * NB_RECORD_FOR_DAY)
+for key in keywords:
+    indice = df_max.index.get_loc(key)
+    plt.plot(
+        abscisse,
+        haty[indice, -nb_days_show * NB_RECORD_FOR_DAY :],
+        label="prédiction",
+        color="red",
+    )
+    plt.plot(
+        abscisse,
+        goldy[indice, -nb_days_show * NB_RECORD_FOR_DAY :],
+        label="vrai valeur",
+        color="green",
+    )
+    plt.savefig(f"./plot/prev_{key}.png", dpi=1000)
+    plt.legend(loc="upper left")
+    plt.clf()
